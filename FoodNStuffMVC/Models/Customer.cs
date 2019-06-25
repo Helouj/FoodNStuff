b99c4f8be89ce2923ba4ddef1011de34d7d82d1e
Helouj <JacobHelouZX@gmail.com>
2019-06-25 10:52:37 -0400

    added first virtual function

---
+++ FoodNStuffMVC/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodNStuffMVC.Models
+{
+    public class Customer
+    {
+        [Key]
+
+        public int CustomerID { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+
+            get
+            {
+                //public string FullName => $"{FirstName} {LastName}";
+                return $"{FirstName} {LastName}";
+                
+
+            }
+
+        }
+
+
+    }
+}
---
+++ FoodNStuffMVC/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FoodNStuffMVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+        
+        public int CustomerID { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        public virtual Product Product { get; set; }
+
+        public int ProductID { get; set; }
+
+
+    }
+}
